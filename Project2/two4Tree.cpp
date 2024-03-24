e214d95cb449e01ca523e1fa6c8383a1a99913af
calebdochow <calebdochow@gmail.com>
2024-03-23 23:33:19 -0500

    fml

--- Project2/two4Tree.cpp
+++ Project2/two4Tree.cpp
@@ -28,18 +28,33 @@ class Element {
 template <typename key, typename value>
 class Node {
     public:
-        CircularDynamicArray<Element<key, value>> elements; //DATA
+        Element<key, value> elements[3];
 
-        Node* children[4];
+        Node *children[4];
         Node* parent;
 
-        bool leaf; 
+        bool leaf;
+        int kN;
+        int kNS; 
 
         Node() { //Constructor
             children[0] = nullptr;
             children[1] = nullptr;
             children[2] = nullptr;
             children[3] = nullptr;
+            kN = 0;
+            kNS = 0;
+        }
+
+        Node(key k, value v) { //Constructor
+            Element<key, value> element(k, v);
+            elements[0] = element;
+            children[0] = nullptr;
+            children[1] = nullptr;
+            children[2] = nullptr;
+            children[3] = nullptr;
+            kN = 1;
+            kNS = 1;
         }
 };
 
@@ -50,19 +65,73 @@ class two4Tree {
         Node<keytype, valuetype> *root; 
         
         two4Tree() {
-            root = new Node<keytype, valuetype>();
+            Node<keytype, valuetype> *x = new Node<keytype, valuetype>();
+            x->leaf = true;
+            x->kN = 0;
+            x->kNS = 0;
+            root = x;
         }
 
         two4Tree(keytype k[], valuetype V[], int s) {
-            root = new Node<keytype, valuetype>();
+            Node<keytype, valuetype> *x = new Node<keytype, valuetype>();
+            root = x;
+            x->kN = 0;
+            x->kNS = 0;
 
             for(int i = 0; i < s; i++){
                 insert(k[i], V[i]);
+                cout << "Key: " << k[i] << " Value: " << V[i] << endl;
             }
         }
 
+        two4Tree(const two4Tree& other){
+            Node<keytype, valuetype> *x = new Node<keytype, valuetype>();
+            x->leaf = true;
+            x->kN = 0;
+            x->kNS = 0;
+            this->root;
+            this->root = copy(other.root);
+        }
+
         ~two4Tree() {
-            deconstructTree();
+            deconstruct(root);
+        }
+
+        void deconstruct(Node<keytype, valuetype> *node){
+            if(node){
+                for(int i = 0; i < node->kN + 1; i++){
+                    deconstruct(node->children[i]);
+                }
+                delete node;
+            }
+        }
+
+        two4Tree& operator=(const two4Tree& other){
+            if(this != &other){
+                Node<keytype, valuetype> *x = new Node<keytype, valuetype>();
+                x->leaf = true;
+                x->kN = 0;
+                x->kNS = 0;
+                root = x;
+                root = copy(other.root);
+            }
+            return *this;
+        }
+
+        Node<keytype, valuetype> * copy(Node<keytype, valuetype>* node){
+            if(!node){
+                return nullptr;
+            }
+            Node<keytype, valuetype>* newNode = new Node<keytype, valuetype>;
+            newNode->elements = node->elements;
+            newNode->leaf = node->leaf;
+            newNode->kN = node->kN;
+            newNode->kNS = node->kNS;
+
+            for(int i = 0; i < node->kN+1; i++){
+                newNode->children[i] = copy(node->children[i]);
+            }
+            return newNode;
         }
 
         valuetype * search(keytype k) { //merge down
@@ -73,8 +142,8 @@ class two4Tree {
                     i = i + 1;
                 }
                 if(i <= x->elements.length() && k == x->elements[i].key){
-                    return ()
-                }else if(x.leaf){
+                    return 0;
+                }else if(x->leaf){
                     return NULL;
                 }else{
                     x = x->children[i];
@@ -83,65 +152,17 @@ class two4Tree {
         }
 
         void insert(const keytype& key, const valuetype& value) {
-            // Call a helper function to recursively insert the key-value pair
-            insertRecursive(root, key, value);
-            size++; // Increment the size of the tree
+            
         }
 
-        void insertRecursive(Node<keytype, valuetype>* node, const keytype& key, const valuetype& value) {
-            // Check if the current node is a leaf node
-            if (node->leaf) {
-                // Insert the key-value pair into the current node
-                if (node->size < 3) {
-                    // If there is space in the node, simply insert the pair
-                    Element<keytype, valuetype> element;
-                    element.setKey(key, value);
-                    node->elements.addEnd(element);
-                    node->size++;
-                } else {
-                    // Otherwise, split the node and recursively insert into the appropriate child
-                    // (you'll need to implement the splitting logic)
-                }
-            } else {
-                // Find the appropriate child to recursively insert into
-                // (you'll need to implement the search logic)
-            }
-        }
 
         void insertNonefull(Node<keytype, valuetype> *node, keytype k, valuetype v) {
-
-            int i = node->elements.size();
 
         }
 
         void split(Node<keytype, valuetype> *node, int i) {
-            Node<keytype, valuetype> *z = new Node<keytype, valuetype>;
-            Node<keytype, valuetype> *y;
-
-            if (i == 0){
-                y = node->left;
-            } else if (i == 1) {
-                y = node->middeLeft;
-            } else if (i == 2) {
-                y = node->middleRight;
-            } else if (i == 3) {
-                y = node->right;
-            }
-
-            z->leaf = y->leaf;
-
-            z->size = 1;
-
+           
         }
 
-        void deconstructTree(Node<keytype, valuetype>* node){
-            if(node){
-                for(int i = 0; i < node->size + 1; i++){
-                    deconstructTree(node->children[i]);
-                }
-
-                delete node;
-            }
-        }
 
 };
